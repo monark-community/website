7017cdae850ab43fec89f58026d32ebbd19a66f8
unknown <me@dominicfournier.com>
2025-02-20 12:04:08 -0500

    Attempt to fix metadata

--- app/[locale]/(website)/page.tsx
+++ app/[locale]/(website)/page.tsx
@@ -8,7 +8,9 @@ import { Locale } from "@/i18n.config";
 
 type Params = Promise<{ locale: Locale }>;
 
-export const generateMetadata = await metadata;
+export async function generateMetadata() {
+  return metadata;
+}
 
 export default async function Home({ params }: { params: Params }) {
   const { locale } = await params;
