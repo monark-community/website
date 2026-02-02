e5fb59f642da9192b2afaa556e267197b56abdd0
Dominic Fournier <me@dominicfournier.com>
2026-02-02 13:02:01 -0500

    feat - add vaulta partner in hero.

--- data/partners.ts
+++ data/partners.ts
@@ -1,5 +1,10 @@
 const PARTNERS = [
   {
+    id: "vaulta-network",
+    name: "Vaulta",
+    url: "https://www.vaulta.com/",
+  },
+  {
     id: "pinax-network",
     name: "Pinax",
     url: "https://www.pinax.network/",
